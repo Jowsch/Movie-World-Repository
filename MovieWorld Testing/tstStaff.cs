f191871febe7d16a79dab2e83d99d5e27649e74a
Hussein <p2507266@GWHXX679WIN003W>
2020-03-05 17:52:10 +0000

    Added testing and class for staff mgmt

---
+++ MovieWorld Testing/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieWorldClasses;
+
+namespace MovieWorld_Testing
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsStaff AStaff = new clsStaff();
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
