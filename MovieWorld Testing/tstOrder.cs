06ff754f4edce9280995e9d6f26c49b20d43749a
Ubaydullah Ahmad <P2401516@my365.dmu.ac.uk>
2020-03-10 14:42:48 +0000

    first 3 fields done

--- MovieWorld Testing/tstOrder.cs
+++ MovieWorld Testing/tstOrder.cs
@@ -5,7 +5,7 @@ using MovieWorldClasses;
 namespace MovieWorld_Testing
 {
     [TestClass]
-    public class tstOrder
+    public class TstOrder
     {
         [TestMethod]
         public void InstanceOK()
@@ -13,6 +13,34 @@ namespace MovieWorld_Testing
 
             clsOrder Anorder = new clsOrder();
             Assert.IsNotNull(Anorder);
+        }
+        [TestMethod]
+        public void OrderIdPropertyOk()
+        {
+            clsOrder Anorder = new clsOrder();
+            int TestData = 1;
+
+            Anorder.Order_id = TestData;
+            Assert.AreEqual(Anorder.Order_id, TestData);
+        }
+        [TestMethod]
+        public void DateOfOrderPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+
+            AnOrder.create_date = TestData;
+            Assert.AreEqual(AnOrder.create_date, TestData);
+        }
+        [TestMethod]
+        public void CustomerIdPropertyOK()
+        {
+            clsOrder Anorder = new clsOrder();
+            string TestData = "Joe";
+
+            Anorder.Customer_Id = TestData;
+            Assert.AreEqual(Anorder.Customer_Id, TestData);
+
         }
     }
 }
--- MovieWorldClasses/clsOrder.cs
+++ MovieWorldClasses/clsOrder.cs
@@ -1,6 +1,21 @@
-﻿namespace MovieWorldClasses
+﻿using System;
+
+namespace MovieWorldClasses
 {
     public class clsOrder
     {
+        private int OrderNo;
+        private int CustomerId;
+        private DateTime DateOfOrder;
+        private string TotalCost;
+        private string StaffId;
+        private bool AvailableSeats;
+
+        public int Order_id { get; set; }
+        public DateTime create_date { get; set; }
+        public string Customer_Id { get; set; }
     }
-}
+      
+    }
+
+
