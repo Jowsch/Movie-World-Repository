5561a59831ff106cd60d4d92bb8e9f69a7be3c3a
Jowsch <Jowsch@users.noreply.github.com>
2020-03-20 13:52:06 +0000

    Finsihed week 23 Collection class

---
+++ MovieWorldFrontOffice/CustomerList.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CustomerList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack == false)
+        {
+            DisplayCustomers();
+        }
+    }
+
+    void DisplayCustomers()
+    {
+        MovieWorldClasses.clsCustomerCollection Customers = new MovieWorldClasses.clsCustomerCollection();
+        lstCustomerList.DataSource = Customers.CustomerList;
+        lstCustomerList.DataValueField = "customer_id";
+        lstCustomerList.DataTextField = "email";
+        lstCustomerList.DataBind();
+    }
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
