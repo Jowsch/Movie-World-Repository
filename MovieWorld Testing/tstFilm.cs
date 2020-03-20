000f5f78ef554434d313da44eed1271cbb7393d4
Frazer Eastman <P2414199@my365.dmu.ac.uk>
2020-03-20 19:27:52 +0000

    week 19 work

---
+++ MovieWorld Testing/tstFilm.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieWorldClasses;
+using System;
+
+namespace MovieWorld_Testing
+{
+    [TestClass]
+    public class tstFilm
+    {
+        [TestMethod]
+        public void DescriptionPropertyOk()
+        {
+            clsFilm film = new clsFilm();
+            String TestData = "a film about...";
+            film.FilmDescription = TestData;
+            Assert.AreEqual(film.FilmDescription, TestData);
+        }
+
+        [TestMethod]
+        public void CertificatePropertyOK()
+        {
+            clsFilm film = new clsFilm();
+            String TestData = "U";
+            film.FilmCertificate = TestData;
+            Assert.AreEqual(film.FilmCertificate, TestData);
+        }
+
+        [TestMethod]
+        public void ReleaseDatePropertyOK()
+        {
+            clsFilm film = new clsFilm();
+            DateTime TestData = DateTime.Now.Date;
+            film.FilmReleaseDate = TestData;
+            Assert.AreEqual(film.FilmReleaseDate, TestData);
+        }
+
+        [TestMethod]
+        public void DepartureDatePropertyOK()
+        {
+            clsFilm film = new clsFilm();
+            DateTime TestData = DateTime.Now.Date;
+            film.FilmDepartureDate = TestData;
+            Assert.AreEqual(film.FilmDepartureDate, TestData);
+        }
+
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            clsFilm film = new clsFilm();
+            String TestData = "U";
+            film.FilmName = TestData;
+            Assert.AreEqual(film.FilmName, TestData);
+        }
+
+        [TestMethod]
+        public void ShowingPropertyOK()
+        {
+            clsFilm film = new clsFilm();
+            Boolean TestData = true;
+            film.FilmShowing = TestData;
+            Assert.AreEqual(film.FilmShowing, TestData);
+        }
+    }
+}
---
+++ MovieWorldClasses/clsFilm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MovieWorldClasses
+{
+    public class clsFilm
+    {
+        public string FilmDescription { get; set; }
+        public string FilmCertificate { get; set; }
+        public DateTime FilmReleaseDate { get; set; }
+        public DateTime FilmDepartureDate { get; set; }
+        public string FilmName { get; set; }
+        public bool FilmShowing { get; set; }
+        public string FilmID { get; set; }
+    }
+}
