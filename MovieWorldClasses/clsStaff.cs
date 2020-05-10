a16e2331430eec5901f8c882577f7d7f62fb9af8
Hussein Ahmed <P2507266@my365.dmu.ac.uk>
2020-05-10 15:36:46 +0100

    tstStaff done with table

--- MovieWorldClasses/clsStaff.cs
+++ MovieWorldClasses/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace MovieWorldClasses
+﻿using System;
+
+namespace MovieWorldClasses
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public bool StaffActive { get; set; }
+        public DateTime StaffDOB { get; set; }
+        public int StaffID { get; set; }
+        public int StaffPhoneNo { get; set; }
+        public object StaffName { get; set; }
+        public object StaffSalary { get; set; }
     }
 }
