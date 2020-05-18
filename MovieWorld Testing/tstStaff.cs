0806456dce88f24e7af7ad7d794177727bbe09cb
Hussein Ahmed <P2507266@my365.dmu.ac.uk>
2020-05-18 08:23:53 +0100

    Final attempt at wk23 without presentation layer

--- MovieWorld Testing/tstStaff.cs
+++ MovieWorld Testing/tstStaff.cs
@@ -7,6 +7,13 @@ namespace MovieWorld_Testing
     [TestClass]
     public class tstStaff
     {
+        string StaffName = "Hussein Ahmed";
+        string StaffPhoneNo = "7475166277";
+        string StaffSalary = "2000";
+        string StaffDOB = "01/07/1995";
+
+        private int StaffID;
+
         [TestMethod]
         public void InstanceOk()
         {
@@ -92,5 +99,216 @@ namespace MovieWorld_Testing
             //test to see that the two values are the same
             Assert.AreEqual(AStaff.StaffSalary, TestData);
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Int32 StaffID = 1;
+            Found = AStaff.Find(StaffID);
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestStaffIDFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StaffID = 21;
+            Found = AStaff.Find(StaffID);
+            if (AStaff.StaffID != 21)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffDOBFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Found = AStaff.Find(StaffID);
+            if (AStaff.StaffDOB != Convert.ToDateTime("16/09/2015"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestStaffNameFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check the property
+            if (AStaff.StaffName != "Test Name")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffSalaryFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check the property
+            if (AStaff.StaffSalary != "Test Salary")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffPhoneNoFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check the property
+            if (AStaff.StaffPhoneNo != 1)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+      
+
+        [TestMethod]
+        public void StaffActiveFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AStaff.Find(StaffID);
+            //check the property
+            if (AStaff.StaffActive != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            String Error = "";
+            Error = AStaff.Valid(StaffName, StaffPhoneNo, StaffSalary, StaffDOB);
+            Assert.AreEqual(Error, "");
+        }
+
+        public void StaffNameMin()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string StaffName = "a"; //this should be ok
+            //invoke the method
+            Error = AStaff.Valid(StaffName, StaffPhoneNo, StaffSalary, StaffDOB);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void StaffNameMinPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string StaffName = "aa"; //this should be ok
+            //invoke the method
+            Error = AStaff.Valid(StaffName, StaffPhoneNo, StaffSalary, StaffDOB);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void StaffNameMaxLessOne()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string StaffName = "aaaaa"; //this should be ok
+            //invoke the method
+            Error = AStaff.Valid(StaffName, StaffPhoneNo, StaffSalary, StaffDOB);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+        public void StaffNameMax()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string AStaff = "aaaaaa"; //this should be ok
+            //invoke the method
+            Error = AStaff.Valid(StaffName, StaffPhoneNo, StaffSalary, StaffDOB);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void HouseNoMid()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string StaffName = "aaa"; //this should be ok
+            //invoke the method
+            Error = AStaff.Valid(StaffName, StaffPhoneNo, StaffSalary, StaffDOB}
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
     }
 }
--- MovieWorldClasses/clsStaff.cs
+++ MovieWorldClasses/clsStaff.cs
@@ -1,15 +1,125 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace MovieWorldClasses
 {
     public class clsStaff
     {
-        public bool Active { get; set; }
-        public bool StaffActive { get; set; }
-        public DateTime StaffDOB { get; set; }
-        public int StaffID { get; set; }
-        public int StaffPhoneNo { get; set; }
-        public object StaffName { get; set; }
-        public object StaffSalary { get; set; }
+        private Boolean mStaffActive;
+        public bool StaffActive
+        {
+            get
+            {
+                return mStaffActive;
+      
+            }
+            set
+            {
+                mStaffActive = value;
+            }
+        }
+
+        private int mStaffPhoneNo;
+        public int StaffPhoneNo
+        {
+            get
+            {
+                return mStaffPhoneNo;
+            }
+            set
+            {
+                mStaffPhoneNo = value;
+            }
+        }
+
+        private object mStaffName;
+        public object StaffName
+        {
+            get
+            {
+                return mStaffName;
+
+            }
+            set
+            {
+                mStaffName = value;
+            }
+        }
+
+        private object mStaffSalary;
+        public object StaffSalary
+        {
+            get
+            {
+                return mStaffSalary;
+            }
+            set
+            {
+                mStaffSalary = value;
+            }
+        }
+
+        public bool Find(int StaffID)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@StaffID", StaffID);
+            DB.Execute("sproc_tblStaffManagement_FilterByStaffID");
+            if (DB.Count == 1)
+            {
+                mStaffID = Convert.ToInt32(DB.DataTable.Rows[0]["StaffID"]);
+                mStaffDOB = Convert.ToDateTime(DB.DataTable.Rows[0]["StaffDOB"]);
+                mStaffName = Convert.ToString(DB.DataTable.Rows[0]["StaffName"]);
+                mStaffPhoneNo = Convert.ToInt32(DB.DataTable.Rows[0]["StaffPhoneNo"]);
+                mStaffSalary = Convert.ToDecimal(DB.DataTable.Rows[0]["StaffSalary"]);
+                mStaffActive = Convert.ToBoolean(DB.DataTable.Rows[0]["StaffActive"]); ;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        private Int32 mStaffID;
+        public Int32 StaffID
+        {
+            get
+            {
+                return mStaffID;
+            }
+            set
+            {
+                mStaffID = value;
+            }
+        }
+
+        private DateTime mStaffDOB;
+        public DateTime StaffDOB
+        {
+            get
+            {
+                return mStaffDOB;
+            }
+            set
+            {
+                mStaffDOB = value;
+            }
+        }
+
+        public bool Find(object staffID)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        
+public string Valid(string staffName, string staffPhoneNo, string staffSalary, string staffDOB)
+        {
+            return "";
+        }
     }
+
 }
